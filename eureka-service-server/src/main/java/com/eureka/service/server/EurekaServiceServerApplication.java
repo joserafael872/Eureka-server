433304dd4d45f4a20a795f95f6b814931ddcde5b
1613667 <1613667@22hw035077.SOAM.TCS.com>
2020-04-21 15:52:27 -0500

    first commit

---
+++ eureka-service-server/src/main/java/com/eureka/service/server/EurekaServiceServerApplication.java
@@ -0,0 +1,14 @@
+package com.eureka.service.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceServerApplication.class, args);
+	}
+}
